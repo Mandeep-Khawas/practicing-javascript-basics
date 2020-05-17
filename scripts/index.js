c258745b529218b406f794772b0dc3889dc73869
Mandeep-Khawas <topgun.mandeep@gmail.com>
2020-05-17 18:27:42 +1000

    'AddedOtherButtons'

--- scripts/index.js
+++ scripts/index.js
@@ -8,5 +8,45 @@ document.querySelector('#addButton').addEventListener('click',() => {
 
 	const resultElement = document.createElement('p');
 	resultElement.textContent = `The sum of ${num1} and ${num2} is ${result}.`;
+	document.querySelector('#resultBox').innerHTML = '';
 	document.querySelector('#resultBox').appendChild(resultElement);
+})
+
+document.querySelector("#subtractButton").addEventListener('click', () => {
+	const num1 = parseInt(document.querySelector('#firstNumBox').value);
+	const num2 = parseInt(document.querySelector('#secondNumBox').value);
+	
+	const subtraction = (n1, n2) => {return n1 - n2;};
+	const subtractResult = subtraction(num1, num2);
+	
+	const subtractResultElement = document.createElement('p');
+	subtractResultElement.textContent = `The difference between ${num1} and ${num2} is ${subtractResult}.`;
+	document.querySelector('#resultBox').innerHTML = '';
+	document.querySelector('#resultBox').appendChild(subtractResultElement);
+})
+
+document.querySelector('#multiplyButton').addEventListener('click', () => {
+	const num1 = parseInt(document.querySelector('#firstNumBox').value);
+	const num2 = parseInt(document.querySelector('#secondNumBox').value);
+	
+	const multiplication = (n1, n2) => {return n1 * n2;};
+	const multiplyResult = multiplication(num1, num2);
+	
+	const multiplyResultElement = document.createElement('p');
+	multiplyResultElement.textContent = `The product of${num1} and ${num2} is ${multiplyResult}.`;
+	document.querySelector('#resultBox').innerHTML = '';
+	document.querySelector('#resultBox').appendChild(multiplyResultElement);
+});
+
+document.querySelector('#divideButton').addEventListener('click', () => {
+	const num1 = parseInt(document.querySelector('#firstNumBox').value);
+	const num2 = parseInt(document.querySelector('#secondNumBox').value);
+	
+	const division = (n1, n2) => {return n1/n2;};
+	const divisionResult = division(num1, num2);
+	
+	const divisionResultElement = document.createElement('p');
+	divisionResultElement.textContent = `${num1} divided by ${num2} is ${divisionResult}.`;
+	document.querySelector('#resultBox').innerHTML  = "";
+	document.querySelector('#resultBox').appendChild(divisionResultElement);
 })
